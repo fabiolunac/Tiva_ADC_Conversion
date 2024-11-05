021431f7bf76ba9cc69896bf907c3df35cbbf147
Fábio Luna <fabioluna20@gmail.com>
2024-11-05 14:10:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ import serial
 import numpy as np
 import matplotlib.pyplot as plt
 
-# # Configurar a porta serial
+# # Configurar a porta serial 
 porta_serial = serial.Serial('COM5', baudrate=115200, timeout=1)
-N = 100
-Fs = 500
+
+Nc = 4 #numero de ciclos do sinal
+
+Fs = 10000
+f_signal = 200
+f_test = 200
+
+N = int((Fs/f_signal)*Nc) #numero de pontos do buffer
+
+ADC_factor = int(4095/3.3)
 
 def send_int_to_uart(serialobj: serial.Serial, value: int, num_bytes: int, byteorder: str) -> None:
     bytes_to_send = value.to_bytes(num_bytes, byteorder, signed=True)
@@ -26,9 +34,37 @@ def read_int_vector(serialobj: serial.Serial, num_elements: int) -> np.ndarray:
 
     return signal
 
+def signal_FFT(signal: int) -> None:
+    y = np.array(y) / ADC_factor
+
+    Y = np.fft.fft(y)
+
+    y_fft_magnitude = np.abs(Y)[:N // 2] 
+    freqs = np.fft.fftfreq(N, 1 / Fs)[:N // 2]  
+
+    plt.figure()
+    plt.subplot(211)
+    plt.plot(y)
+    plt.grid()
+    plt.title(f'Sinal Senoidal, f = {f_test} Hz')
+    plt.ylabel("Amplitude (V)")
+
+    plt.subplot(212)
+    plt.stem(freqs, y_fft_magnitude*2/N)
+    plt.axvline(x = f_test, color = 'red', linestyle = '--', label = f'Frequência do Sinal, f = {f_test} Hz')
+    plt.grid()
+    plt.title("FFT")
+    plt.legend()
+    plt.xlabel("Frequência (Hz)")
+    plt.ylabel("Magnitude")
+
+    plt.tight_layout()
+    plt.show()
+
+
 while True:
     try:
-        x = input("Comandos: 1 - Iniciar Transferência | 0 - Não iniciar: ")
+        x = input("1 - Iniciar Transferência\n0 - Não iniciar: ")
 
         if x == '0':
             send_int_to_uart(porta_serial, int(x), 4, 'little')
@@ -39,26 +75,7 @@ while True:
             send_int_to_uart(porta_serial, int(x), 4, 'little')
             y = read_int_vector(porta_serial, N)
 
-            Y = np.fft.fft(y)
-
-            y_fft_magnitude = np.abs(Y)[:N // 2]  # Magnitude da FFT (apenas metade positiva)
-            freqs = np.fft.fftfreq(N, 1 / Fs)[:N // 2]  # Frequências correspondentes
-
-            plt.figure()
-            plt.subplot(211)
-            plt.plot(y)
-            plt.grid()
-            plt.title('Valores Recebidos')
-
-            plt.subplot(212)
-            plt.stem(freqs, y_fft_magnitude*2/N)
-            plt.grid()
-            plt.title("FFT")
-            plt.xlabel("Frequência (Hz)")
-            plt.ylabel("Magnitude")
-
-            plt.tight_layout()
-            plt.show()
+            signal_FFT(y)
 
         else:
             print('Comando inválido!\nDigite 1 para iniciar ou 0 para sair\n')
